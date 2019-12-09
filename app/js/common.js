4cbe8d416d2fdfad696e92153c1ff92a0e78dbde
eugenejs <57704927+delictt@users.noreply.github.com>
2019-12-09 15:49:59 +0300

    home - achivements

--- app/js/common.js
+++ app/js/common.js
@@ -9,12 +9,6 @@ $( document ).ready(function() {
 
 	if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9'})
 
-	$(window).scroll(function (){
-		if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9', 'transition' : '.5s'})
-		else $('.header__top').css({'box-shadow' : '0 0 0 #D9D9D9', 'transition' : '.5s'})
-	});
-
-
 	$(window).resize(function(){
 		if ($('.hamburger').hasClass('is-active')) $('.hamburger').click()
 	});
@@ -33,36 +27,39 @@ $( document ).ready(function() {
 	  );
 	  wow.init();
 
+});
 
-	  var a = 0;
-	  $(window).scroll(function() {
+var a = 0;
+$(window).scroll(function(){
 
-		var oTop = $('#counter').offset().top - window.innerHeight;
-		if (a == 0 && $(window).scrollTop() > oTop) {
-		  $('.counter-value').each(function() {
-			var $this = $(this),
-			  countTo = $this.attr('data-count');
-			$({
-			  countNum: $this.text()
-			}).animate({
-				countNum: countTo
-			  },
+	var oTop = $('.achivements__item').offset().top - window.innerHeight;
+	if (a == 0 && $(window).scrollTop() > oTop) {
+		$('.counter').each(function() {
+		var $this = $(this),
+			countTo = $this.attr('data-count');
+		$({
+			countNum: $this.text()
+		}).animate({
+			countNum: countTo
+			},
 
-			  {
+			{
 
-				duration: 2000,
-				easing: 'swing',
-				step: function() {
-				  $this.text(Math.floor(this.countNum));
-				},
-				complete: function() {
-				  $this.text(this.countNum);
-				}
+			duration: 1500,
+			easing: 'swing',
+			step: function() {
+				$this.text(Math.floor(this.countNum));
+			},
+			complete: function() {
+				$this.text(this.countNum);
+			}
 
-			  });
-		  });
-		  a = 1;
-		}
+			});
+		});
+		a = 1;
+	}
 
-	  });
+	if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9', 'transition' : '.5s'})
+	else $('.header__top').css({'box-shadow' : '0 0 0 #D9D9D9', 'transition' : '.5s'})
+
 });
