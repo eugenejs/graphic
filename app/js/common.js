78a99afa9ac74b57fa835688e65667772b2ba0ce
eugenejs <57704927+delictt@users.noreply.github.com>
2019-12-11 18:56:13 +0300

    add content

--- app/js/common.js
+++ app/js/common.js
@@ -1,24 +1,35 @@
 $( document ).ready(function() {
 
 		$('.owl-carousel').owlCarousel({
-			items : 3,
+			items : 1,
 			loop : true,
 			dots: false,
-			nav: true
+			nav: true,
+			responsiveClass:true,
+			navText : ["<i class='fas fa-long-arrow-alt-left'>","<i class='fas fa-long-arrow-alt-right'></i>"],
+			responsive:{
+				968:{
+					items: 3,
+
+				},
+				768:{
+					items: 2
+				}
+			}
 		});
 
-// 	$('.hamburger').click(function (){
-// 		$('.hamburger').toggleClass('is-active')
-// 		if($('.hamburger').hasClass('is-active')) $('.menu').css({'left' : '0%', 'transition' : '.3s ease-out'})
-// 		else $('.menu').css({'left' : '-100%', 'transition' : '.3s ease-out'})
-// 	});
+	$('.hamburger').click(function (){
+		$('.hamburger').toggleClass('is-active')
+		if($('.hamburger').hasClass('is-active')) $('.navigation').css({'left' : '0%', 'transition' : '.3s ease-out'})
+		else $('.navigation').css({'left' : '-100%', 'transition' : '.3s ease-out'})
+	});
 
 
 // 	if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9'})
 
-// 	$(window).resize(function(){
-// 		if ($('.hamburger').hasClass('is-active')) $('.hamburger').click()
-// 	});
+	$(window).resize(function(){
+		if ($('.hamburger').hasClass('is-active')) $('.hamburger').click()
+	});
 
 
 // 	var wow = new WOW(
