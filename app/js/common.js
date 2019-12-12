407111b06d1e65f6e261e79958df9bd4c7b01633
eugenejs <57704927+delictt@users.noreply.github.com>
2019-12-12 16:40:12 +0300

    add hamburger and responsive menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,34 +1,30 @@
 $( document ).ready(function() {
 
-		$('.owl-carousel').owlCarousel({
-			items : 1,
-			loop : true,
-			dots: false,
-			nav: true,
-			responsiveClass:true,
-			navText : ["<i class='fas fa-long-arrow-alt-left'>","<i class='fas fa-long-arrow-alt-right'></i>"],
-			responsive:{
-				968:{
-					items: 3,
+	$('.owl-carousel').owlCarousel({
+		items : 1,
+		loop : true,
+		dots: false,
+		nav: true,
+		responsiveClass:true,
+		navText : ["<i class='fas fa-long-arrow-alt-left'>","<i class='fas fa-long-arrow-alt-right'></i>"],
+		responsive:{
+			968:{
+				items: 3,
 
-				},
-				768:{
-					items: 2
-				}
+			},
+			768:{
+				items: 2
 			}
-		});
-
-	$('.hamburger').click(function (){
-		$('.hamburger').toggleClass('is-active')
-		if($('.hamburger').hasClass('is-active')) $('.navigation').css({'left' : '0%', 'transition' : '.3s ease-out'})
-		else $('.navigation').css({'left' : '-100%', 'transition' : '.3s ease-out'})
+		}
 	});
 
+	$('.hamburger').click(function (){
 
-// 	if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9'})
+		$('.hamburger').toggleClass('is-active')
 
-	$(window).resize(function(){
-		if ($('.hamburger').hasClass('is-active')) $('.hamburger').click()
+		if($('.hamburger').hasClass('is-active')) $('.navigation').css({'left' : '0%', 'transition' : '.3s ease-out'})
+		else $('.navigation').css({'left' : '-50%', 'transition' : '.3s ease-out'})
+
 	});
 
 
@@ -48,8 +44,11 @@ $( document ).ready(function() {
 
 
 
+});
 
+$(window).resize(function (){
 
+	if($('.hamburger').hasClass('is-active')) $('.hamburger').click()
 
 });
 
@@ -83,7 +82,5 @@ $( document ).ready(function() {
 // 		a = 1;
 // 	}
 
-// 	if($(this).scrollTop() > 100) $('.header__top').css({'box-shadow' : '0 0 10px #D9D9D9', 'transition' : '.5s'})
-// 	else $('.header__top').css({'box-shadow' : '0 0 0 #D9D9D9', 'transition' : '.5s'})
 
 // });
